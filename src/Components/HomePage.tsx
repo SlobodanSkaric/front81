6c51dcd92b11d64b980944bde33d751a32fdfaf9
SlobodanSkaric <softverss@gmail.com>
2025-12-07 17:09:25 +0100

    Implement login pages

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
@@ -5,9 +6,9 @@ export default function HomePage() {
     <div className="grid grid-cols-12 min-h-screen gap-2">
       <div className="col-span-2  bg-gray-300 text-cyan-700 p-4 flex flex-col justify-start gap-1">
         <h2 className="text-2xl font-bold mb-4 border-cyan-700 border-b-2">Inco Print</h2>
-        <div className='hover:cursor-pointer hover:bg-neutral-200 font-sans hover:rounded-xl pl-3 boreder border  transition-all duration-200'><a href="/info">Info</a></div>
-        <div className='hover:cursor-pointer hover:bg-neutral-200 font-sans hover:rounded-xl pl-3 boreder border  transition-all duration-200'><a href="/list">Lista Poslova</a></div>
-        <div className='hover:cursor-pointer hover:bg-neutral-200 font-sans hover:rounded-xl pl-3 boreder border  transition-all duration-200'><a href="/login">Prijava Na Sistem</a></div>
+        <Link  className='hover:cursor-pointer hover:bg-neutral-200 font-sans hover:rounded-xl pl-3 boreder border  transition-all duration-200' to="/info">Info</Link>
+        <Link  className='hover:cursor-pointer hover:bg-neutral-200 font-sans hover:rounded-xl pl-3 boreder border  transition-all duration-200' to="/joblist">Lista Poslova</Link>
+        <Link  className='hover:cursor-pointer hover:bg-neutral-200 font-sans hover:rounded-xl pl-3 boreder border  transition-all duration-200' to="/login">Prijavi Se</Link>
       </div>
       <div className="col-span-10 grid grid-rows-2 gap-4">
         <div className="bg-gray-100 text-black p-4 flex items-center justify-center">
--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useCallback, useState, type ChangeEvent, type FormEvent } from "react";
 
-export default function Login() {
+interface LoginProps {
+    email?: string;  
+    password?: string;
+}
+
+const LoginForm: React.FC<LoginProps> = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+
+  const handleSubmit = (e: FormEvent) => {
+      e.preventDefault();
+      setError(null);
+      setLoading(true);      
+    };
+
+  const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>, setter: React.Dispatch<React.SetStateAction<string>>) =>{
+    setter(e.target.value);
+  }, []);
+
+
   return (
-    <div>Login</div>
+   <>
+    <div className='flex flex-col items-center justify-center bg-gray-100 min-h-screen p-7'>
+        <div className='w-full max-w-sm p-8 bg-white rounded-xl shadow-2xl'>
+            <h2 className='text-2xl font-bold mb-6 text-center text-cyan-700  border-b-cyan-700 border-b-2 pb-2'>Inco Print <br /> <p className="text-sm">Prijava Na Sistem</p></h2>
+
+            {error && (
+              <div className="bg-red-100 border-l-4 border-red-700 text-red-700 p-4 mb-6">
+                <p className="font-bold">Greska</p>
+                <p>{error}</p>
+              </div>
+            )}
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4 ">
+            <label className="block text-cyan-700 text-sm font-semibold mb-2" htmlFor="email">Email Adresa</label>
+            <input 
+              id="email"
+              type="email" 
+              placeholder="Unesite vašu email adresu"
+              className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-700 focus:text-center"
+              value={email}
+              onChange={(e) => handleInputChange(e, setEmail)}
+              required  
+              disabled={loading}
+            />
+          </div>
+          <div className="mb-7">
+            <label className="block text-cyan-700 text-sm font-semibold mb-2" htmlFor="password">Passworsd</label>
+            <input
+              id="password"
+              type="password" 
+              placeholder="Unesite vašu lozinku" 
+              className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-700"
+              value={password}
+              onChange={(e) => handleInputChange(e, setPassword)}
+              required  
+              disabled={loading}
+            />
+          </div>
+          <button
+            type="submit"
+            className={`w-full bg-cyan-700 text-white font-semibold py-2 px-4 rounded-lg hover:bg-cyan-800 transition-colors duration-200 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+            disabled={loading}
+          >
+            {loading ? "Učitavanje..." : "Prijavi Se"}
+          </button>
+        </form>
+        </div>
+    </div>
+   </>
   )
 }
+
+export default LoginForm;
