eaa9be3cbd639690ab9efd4eb88b7035bd4b9b4a
SlobodanSkaric <softverss@gmail.com>
2025-12-11 11:09:11 +0100

    Smale change

--- src/Layouts/AdministratorProfile.tsx
+++ src/Layouts/AdministratorProfile.tsx
@@ -11,7 +11,7 @@ export default function AdministratorProfile() {
   return (
     
     <>
-        <div className="grid grid-cols-12 min-h-screen gap-2">
+        <div className="grid grid-cols-12 min-h-screen ">
             <div className="col-span-2  bg-gray-300 text-cyan-700 p-4 flex flex-col justify-start gap-1">
                 <h2 className="text-2xl font-bold mb-4 border-cyan-700 border-b-2">{username}</h2>
                 <Link  className='hover:cursor-pointer hover:bg-neutral-200 font-sans hover:rounded-xl pl-3 boreder border  transition-all duration-200' to="/adminprofile/showlistjob">Pogledaj Narudzbine</Link>
@@ -22,7 +22,7 @@ export default function AdministratorProfile() {
                 <Link  className='hover:cursor-pointer hover:bg-neutral-200 font-sans hover:rounded-xl pl-3 boreder border  transition-all duration-200' to="/adminprofile/administratorspanel">Administracija</Link>
                 <Link  className='hover:cursor-pointer hover:bg-neutral-200 font-sans hover:rounded-xl pl-3 boreder border  transition-all duration-200' to="/adminprofile/logout">Odjava Sa Sisterema</Link>
             </div>
-            <div className="col-span-10 p-6">
+            <div className="col-span-10 ">
                 <Outlet/>
             </div>
         </div>
--- src/Pages/JobLists.tsx
+++ src/Pages/JobLists.tsx
@@ -4,6 +4,9 @@ export default function JobLists() {
   return (
     
     <>
+      <div className="flex flex-col justify-center items-center bg-gray-100 h-full m-0 p-0 w-full">
+        <div className="">Spisak Poslova</div>
+      </div>
     </>
   )
 }
