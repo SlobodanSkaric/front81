2e70edba9b1ff373e709526855ed464b83c8a016
SlobodanSkaric <softverss@gmail.com>
2025-12-13 17:03:30 +0100

    Add joblist pages and dropdown commponents

---
+++ src/Components/ui/Dropdown.tsx
@@ -0,0 +1,115 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function Dropdown() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [firmName, setFirmName] = useState(false);   
+    const [orderNumber, setOrderNumber] = useState(false);   
+    const [status, setStatus] = useState(false);   
+    const [date, setDate] = useState(false);
+
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    const toogleDropdown = () => {
+        setIsOpen(!isOpen);
+    }
+
+    useEffect(() => {
+        const handeleCliskOutside = (event: MouseEvent) =>{
+                if(menuRef.current && !menuRef.current.contains(event.target as Node)){
+                    setIsOpen(false);
+                    setFirmName(false);
+                    setOrderNumber(false);
+                    setStatus(false);
+                    setDate(false);
+                }
+             }
+
+            document.addEventListener("mousedown", handeleCliskOutside);
+
+            return () => {
+                document.removeEventListener("mousedown", handeleCliskOutside)
+            }
+           
+       
+    }, []);
+
+    const serchDefinition = (data: any) => {
+        if(data === "f-name"){
+            setFirmName(true);
+            setOrderNumber(false);
+            setStatus(false);
+            setDate(false);
+        }
+        if(data === "f-number"){
+            setFirmName(false);
+            setOrderNumber(true);
+            setStatus(false);
+            setDate(false);
+        }
+        if(data === "f-status"){
+            setFirmName(false);
+            setOrderNumber(false);
+            setStatus(true);
+            setDate(false);
+        }
+        if(data === "f-date"){
+            setFirmName(false);
+            setOrderNumber(false);
+            setStatus(false);
+            setDate(true);
+        }
+    }
+  return (
+    <div className="relative  inline-block  text-left w-full" ref={menuRef}>
+        <div className="flex w-full">
+            <div>
+                <button type="button" 
+                        onClick={toogleDropdown} 
+                        className="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 my-2 bg-cyan-700 text-gray-100 hover:bg-cyan-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-cyan-500 text-sm font-medium">
+                            Pretraga
+                </button>
+            </div>
+            <div className="w-full mr-3 base-">
+                <form action="">
+                    {firmName &&
+                        <input type="text" className="my-2 mx-3 py-1.5 border px-3 border-cyan-700 rounded-xl w-full" placeholder="Pretraga prema nazivu firme"/>
+                    }
+                    {orderNumber &&
+                        <input type="text" className="my-2 mx-3 py-1.5 px-3 border border-cyan-700 rounded-xl w-full" placeholder="Pretraga prema broju narudzbenice"/>
+                    }
+                    {status &&
+                        <input type="text" className="my-2 mx-3 py-1.5 px-3 border border-cyan-700 rounded-xl w-full" placeholder="Pretraga prema statusu"/>
+                    }
+                    {date &&
+                        <input type="text" className="my-2 mx-3 py-1.5 px-3 border border-cyan-700 rounded-xl w-full" placeholder="Pretragaprema datumu"/>
+                    }
+                </form>            
+            </div>
+        </div>
+        {isOpen && (
+            <div 
+          className="origin-top-right absolute left-0  w-3xs rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10 bg-gray-100"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="menu-button"
+          tabIndex={-1}
+        >
+          <div className="text-cyan-700" role="none">
+            <a href="#" onClick={() => serchDefinition("f-name")} className=" block px-4 py-2 text-sm hover:bg-white hover:rounded-md transition-colors duration-150" role="menuitem" tabIndex={-1} id="menu-item-0">
+              Pretraga po imenu firme
+            </a>
+            <a href="#" onClick={() => serchDefinition("f-number")} className=" block px-4 py-2 text-sm hover:bg-white transition-colors duration-150" role="menuitem" tabIndex={-1} id="menu-item-1">
+              Pretraga po broju narudzbine
+            </a>
+            <a href="#" onClick={() => serchDefinition("f-status")} className=" block px-4 py-2 text-sm hover:bg-white transition-colors duration-150" role="menuitem" tabIndex={-1} id="menu-item-1">
+              Pretraga po statusu
+            </a>
+            <a href="#" onClick={() => serchDefinition("f-date")} className=" block px-4 py-2 text-sm hover:bg-white hover:rounded-md transition-colors duration-150" role="menuitem" tabIndex={-1} id="menu-item-1">
+              Pretraga po datumu
+            </a>
+          </div>
+        </div>
+        )}
+    </div>
+  )
+}
--- src/Pages/JobLists.tsx
+++ src/Pages/JobLists.tsx
@@ -1,11 +1,125 @@
+import Dropdown from "../Components/ui/Dropdown";
 
 
 export default function JobLists() {
-  return (
-    
+  return (    
     <>
-      <div className="flex flex-col justify-center items-center bg-gray-100 h-full m-0 p-0 w-full">
-        <div className="">Spisak Poslova</div>
+      <div className="flex flex-col justify-start items-center bg-gray-100 h-full  w-full">
+        <div className="text-2xl font-bold text-cyan-700 my-7">Spisak narudzbina</div>        
+        <div className="  w-11/12">
+        <div className="w-full">
+          <Dropdown />
+        </div>
+          <table className="w-full text-sm text-left bg-cyan-700 text-gray-100 rounded-lg shadow-2xl overflow-hidden   ">
+            <thead className=" bg-gray-100 text-cyan-700 ">
+              <tr>
+                <th scope="col" className="px-6 py-3 font-medium border rounded-lg">
+                    Narucilac
+                </th>
+                <th scope="col" className="px-6 py-3 font-medium border rounded-lg">
+                    Broj Narudzbine
+                </th>
+                <th scope="col" className="px-6 py-3 font-medium border rounded-lg">
+                    Status
+                </th>
+                 <th scope="col" className="px-6 py-3 font-medium border rounded-lg">
+                    Datum Narucivanja
+                </th>
+                <th scope="col" className="px-6 py-3 font-medium border rounded-lg">
+                    Pogledaj Nardzbinu
+                </th>
+            </tr>
+            </thead>  
+
+        <tbody>
+            <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
+                <th scope="row" className="px-6 py-4 font-medium text-heading whitespace-nowrap ">
+                    Apple MacBook Pro 17"
+                </th>
+                <td className="px-6 py-4">
+                    Silver
+                </td>
+                <td className="px-6 py-4">
+                    Laptop
+                </td>
+                <td className="px-6 py-4">
+                    $2999
+                </td>
+                <td className="px-6 py-4">
+                    <a href="#" className="font-medium text-fg-brand hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
+                <th scope="row" className="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                    Microsoft Surface Pro
+                </th>
+                <td className="px-6 py-4">
+                    White
+                </td>
+                <td className="px-6 py-4">
+                    Laptop PC
+                </td>
+                <td className="px-6 py-4">
+                    $1999
+                </td>
+                <td className="px-6 py-4">
+                    <a href="#" className="font-medium text-fg-brand hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
+                <th scope="row" className="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                    Magic Mouse 2
+                </th>
+                <td className="px-6 py-4">
+                    Black
+                </td>
+                <td className="px-6 py-4">
+                    Accessories
+                </td>
+                <td className="px-6 py-4">
+                    $99
+                </td>
+                <td className="px-6 py-4">
+                    <a href="#" className="font-medium text-fg-brand hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
+                <th scope="row" className="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                    Google Pixel Phone
+                </th>
+                <td className="px-6 py-4">
+                    Gray
+                </td>
+                <td className="px-6 py-4">
+                    Phone
+                </td>
+                <td className="px-6 py-4">
+                    $799
+                </td>
+                <td className="px-6 py-4">
+                    <a href="#" className="font-medium text-fg-brand hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft">
+                <th scope="row" className="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                    Apple Watch 5
+                </th>
+                <td className="px-6 py-4">
+                    Red
+                </td>
+                <td className="px-6 py-4">
+                    Wearables
+                </td>
+                <td className="px-6 py-4">
+                    $999
+                </td>
+                <td className="px-6 py-4">
+                    <a href="#" className="font-medium text-fg-brand hover:underline">Edit</a>
+                </td>
+            </tr>
+        </tbody>
+          </table>
+        </div>
       </div>
     </>
   )
